8addbf760211552087584ee72734c5b4ceac0459
fukudaum <fukudaum@gmail.com>
2024-07-04 22:28:13 -0300

    added cors

--- src/chat.gateway.ts
+++ src/chat.gateway.ts
@@ -1,13 +1,21 @@
 import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from "socket.io";
 
-@WebSocketGateway()
+@WebSocketGateway({
+    cors: {
+      origin: '*', // ou a origem específica que você deseja permitir, como 'http://localhost:3000'
+      methods: ['GET', 'POST'],
+      allowedHeaders: ['Content-Type'],
+      credentials: true
+    }
+  })
 export class ChatGateway {
     @WebSocketServer()
     server: Server;
 
     @SubscribeMessage('message')
     handleMessage(@MessageBody() message: string, @ConnectedSocket() client: Socket): void {
+        console.log(message)
         this.server.emit('message', message)
     }
 }
